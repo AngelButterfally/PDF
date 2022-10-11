9e9ec4f745d24b3716d96304e1919854d4cb3b7a
AngelButterfally <1284691263@qq.com>
2022-10-11 19:52:10 +0800

    Merge remote-tracking branch 'origin/main'

--- main.py
+++ main.py
@@ -6,6 +6,7 @@ from PyQt5.QtCore import *
 from Ui_PDF import Ui_MainWindow
 from scanPDF import scan_PDF_function
 
+
 class MainWindow(QtWidgets.QMainWindow, Ui_MainWindow):
     def __init__(self, parent=None):
         super(MainWindow, self).__init__(parent)
@@ -18,7 +19,7 @@ class MainWindow(QtWidgets.QMainWindow, Ui_MainWindow):
         self.setWindowTitle('FUCK!')
         self.setGeometry(300, 300, 1000, 500)
         self.lineEdit.setValidator(QRegExpValidator(QRegExp("[A-Z0-9]+$")))
-    
+
     def iniVariable(self):
         self.count = 0
 
@@ -29,14 +30,14 @@ class MainWindow(QtWidgets.QMainWindow, Ui_MainWindow):
     def show_txt(self):
         self.textEdit.append(str(self.count))
         self.textEdit.append(self.lineEdit.text())
-        self.count = self.count+1
-    
+        self.count = self.count + 1
+
     def scan_PDF(self):
         filename = QFileDialog.getOpenFileName(self, "选择文件", ".", "PDF Files (*.pdf)")
         result = scan_PDF_function(filename[0])
         self.textEdit.append(result)
 
-    
+
 if __name__ == '__main__':
     app = QApplication(sys.argv)
     myWin = MainWindow()
